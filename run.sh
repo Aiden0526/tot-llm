3176f17203af2cb42a656d52b7b42bf1b5dd56eb
Aiden Xu <aidenxu@MacBook-Pro-2.local>
2023-06-17 14:11:18 +0800

    adding comment about the args in sh file

--- run.sh
+++ run.sh
@@ -1,4 +1,4 @@
-API_key="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"
-test_file_path="/Users/aidenxu/Documents/zhangyue_research_intern/test.csv"
+API_key="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"  ## please replace with your openai api
+test_file_path="test.csv" ##please specify the path of the file you want to test
 
 python main.py --api_key $API_key --test_file $test_file_path

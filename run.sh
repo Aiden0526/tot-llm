3e5f852578f4bc6ae59f52e35e91a06a3bc08abc
Aiden Xu <aidenxu@MacBook-Pro-2.local>
2023-06-20 01:21:26 +0800

    commit

--- run.sh
+++ run.sh
@@ -1,4 +1,4 @@
-API_key="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"  ## please replace with your openai api
+API_key="your api key"  ## please replace with your openai api
 test_file_path="test.csv" ##please specify the path of the file you want to test
 
 python main.py --api_key $API_key --test_file $test_file_path
